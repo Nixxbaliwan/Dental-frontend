cab7c7c7573a5488748f080936be9062766ca32f
HutaoMain <alimohamedalcantara@gmail.com>
2023-11-19 13:57:19 +0800

    port

--- src/index.js
+++ src/index.js
@@ -38,7 +38,8 @@ app.use("/api/user", UserRouter);
 app.use("/api/appointment", AppointmentRouter);
 app.use("/api/treatment-record", TreatmentRecordRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   connect();
   console.log(`server is listening to port ${PORT}`);
